ec13f43a19b4feb9841514550f542adeaa6519ba
Emanuele2018 <34986963+Emanuele2018@users.noreply.github.com>
2025-03-30 19:19:52 +0200

    Aspetti iniziali per la creazione fattura

--- Falco/FrmCreaFattura.Designer.cs
+++ Falco/FrmCreaFattura.Designer.cs
@@ -28,8 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             label1 = new Label();
             BtnCrea = new Button();
+            label2 = new Label();
+            cmbClienti = new ComboBox();
+            BtnNuovoCliente = new Button();
+            toolTip1 = new ToolTip(components);
             SuspendLayout();
             // 
             // label1
@@ -55,22 +60,57 @@
             BtnCrea.UseVisualStyleBackColor = true;
             BtnCrea.Click += BtnCrea_Click;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(18, 69);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Cliente:";
+            // 
+            // cmbClienti
+            // 
+            cmbClienti.FormattingEnabled = true;
+            cmbClienti.Location = new Point(24, 95);
+            cmbClienti.Name = "cmbClienti";
+            cmbClienti.Size = new Size(256, 28);
+            cmbClienti.TabIndex = 5;
+            // 
+            // BtnNuovoCliente
+            // 
+            BtnNuovoCliente.Location = new Point(287, 94);
+            BtnNuovoCliente.Name = "BtnNuovoCliente";
+            BtnNuovoCliente.Size = new Size(41, 29);
+            BtnNuovoCliente.TabIndex = 6;
+            BtnNuovoCliente.Text = "...";
+            toolTip1.SetToolTip(BtnNuovoCliente, "Crea un nuovo cliente");
+            BtnNuovoCliente.UseVisualStyleBackColor = true;
+            // 
             // FrmCreaFattura
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(BtnNuovoCliente);
+            Controls.Add(cmbClienti);
+            Controls.Add(label2);
             Controls.Add(BtnCrea);
             Controls.Add(label1);
             Name = "FrmCreaFattura";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Falco - Fattura Elettronica";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Label label1;
         private Button BtnCrea;
+        private Label label2;
+        private ComboBox cmbClienti;
+        private Button BtnNuovoCliente;
+        private ToolTip toolTip1;
     }
 }
