4b141e245167492636c592650121bcb59a1f4d18
Emanuele2018 <34986963+Emanuele2018@users.noreply.github.com>
2025-06-21 12:26:49 +0200

    gestione informazioni

--- Falco/Form1.cs
+++ Falco/Form1.cs
@@ -14,6 +14,13 @@ namespace Falco
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Altri siti
+            //https://www.agenziaentrate.gov.it/portale/normativa-prassi-e-regole-tecniche-fatture-elettroniche
+            //https://www.agenziaentrate.gov.it/portale/web/guest/fogli-di-stile-per-ricevute
+            //https://www.agenziaentrate.gov.it/portale/specifiche-tecniche-versione-1.9
+
+
+            //Altri
             //https://www.agenziaentrate.gov.it/portale/specifiche-tecniche-versione-1.9
             //https://www.agenziaentrate.gov.it/portale/specifiche-tecniche-versione-1.9
             //https://www.agenziaentrate.gov.it/portale/normativa-prassi-e-regole-tecniche-fatture-elettroniche
