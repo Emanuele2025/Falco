198c6102493e437db9d54fd5a9625ddfaea42f8a
Emanuele2025 <emanuelemattei@tutanota.com>
2026-07-16 07:42:44 +0200

    Classe per la gestione di utility

---
+++ Falco/Utility.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Falco
+{
+    public static class Utility
+    {
+
+        #region Messaggi a video
+
+        const string titolo = "Falco";
+        /// <summary>
+        /// Messaggio informativo
+        /// </summary>
+        /// <param name="testo"></param>
+        public static void MessaggioInfo(string testo)
+        {
+            MessageBox.Show(testo, titolo, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        /// <summary>
+        /// MEssaggio di errore
+        /// </summary>
+        /// <param name="test"></param>
+        public static void MessaggioErrore(string testo)
+        {
+            MessageBox.Show(testo, titolo, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        /// <summary>
+        /// Visualizza un messaggio per cancellare il record
+        /// </summary>
+        /// <returns>Restituisce SI se è stata scelta la volontà di cancellare record</returns>
+        public static bool CancellaRecord()
+        {
+            return MessageBox.Show("Si è certi di voler eliminare il record selezionato?", titolo, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes;
+        }
+        /// <summary>
+        /// Messaggio di scelta si e no generico
+        /// </summary>
+        /// <param name="testo"></param>
+        /// <returns></returns>
+        public static bool MessaggioSiNo(string testo)
+        {
+            return MessageBox.Show(testo, titolo, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes;
+        }
+
+
+
+
+
+        #endregion
+
+
+
+
+
+    }
+}
