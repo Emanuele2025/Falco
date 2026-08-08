98f3ff7c9434a79678de37305510f895cf8dd7d8
Emanuele2025 <emanuelemattei@tutanota.com>
2026-08-08 06:38:38 +0200

    gestione fattura

---
+++ Falco/Modelli/DtoCliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Falco.Modelli
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+
+        public string RagioneSociale { get; set; }
+        public string CodiceFiscale { get; set; }
+        public string PartitaIva { get; set; }
+        public string Indirizzo { get; set; }
+        public string Civico { get; set; }
+        public string Cap { get; set; }
+        public string Comune { get; set; }
+        public string Provincia { get; set; }
+        public string Nazione { get; set; }
+        public string Email { get; set; }
+
+        public string PersonaDiRiferimento { get; set; }
+
+
+
+
+    }
+}
