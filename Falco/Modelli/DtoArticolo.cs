80d91736ed71c9a065b591fa8feef85a08e02814
Emanuele2025 <emanuelemattei@tutanota.com>
2026-08-09 07:37:57 +0200

    classi fattura

---
+++ Falco/Modelli/DtoArticolo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Falco.Modelli
+{
+    public class DtoArticolo
+    {
+        public string Descrizione { get; set; }
+        public decimal Quantita { get; set; }
+        public decimal PrezzoUnitario { get; set; }
+        public decimal AliquotaIva { get; set; }
+        public string Natura { get; set; } // N1, N2, N3, N4, N5, N6
+    }
+}
---
+++ Falco/Modelli/DtoProdotti.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Falco.Modelli
+{
+     
+    public class DtoProdotti
+    {
+        public string Descrizione { get; set; }
+        public decimal Quantita { get; set; }
+        public decimal PrezzoUnitario { get; set; }
+        public decimal AliquotaIva { get; set; }
+        public string Natura { get; set; } // N1, N2, N3, N4, N5, N6
+        public string Scorte { get; set; }
+        public string Note { get; set; }
+    }
+}
