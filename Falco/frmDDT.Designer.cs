cec466e998638242c9f533cf4a887d0b7608575c
Emanuele2025 <emanuelemattei@tutanota.com>
2026-08-20 08:26:42 +0200

    Documento di trasporto

---
+++ Falco/frmDDT.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Falco
+{
+    partial class frmDDT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDDT));
+            label1 = new Label();
+            BtnChiudi = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.DeepSkyBlue;
+            label1.Dock = DockStyle.Top;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(1300, 21);
+            label1.TabIndex = 8;
+            label1.Text = "Gestione delle fatture Elettroniche - Gestione documento di Trasporto";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // BtnChiudi
+            // 
+            BtnChiudi.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            BtnChiudi.Image = Properties.Resources.chiudi;
+            BtnChiudi.ImageAlign = ContentAlignment.MiddleLeft;
+            BtnChiudi.Location = new Point(1181, 681);
+            BtnChiudi.Margin = new Padding(3, 2, 3, 2);
+            BtnChiudi.Name = "BtnChiudi";
+            BtnChiudi.Size = new Size(82, 22);
+            BtnChiudi.TabIndex = 15;
+            BtnChiudi.Text = "Chiudi";
+            BtnChiudi.UseVisualStyleBackColor = true;
+            BtnChiudi.Click += BtnChiudi_Click;
+            // 
+            // frmDDT
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1300, 724);
+            Controls.Add(BtnChiudi);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "frmDDT";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Gestione Documento Di Trasprto";
+            Load += frmDDT_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button BtnChiudi;
+    }
+}
---
+++ Falco/frmDDT.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Falco
+{
+    public partial class frmDDT : Form
+    {
+        public frmDDT()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnChiudi_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmDDT_Load(object sender, EventArgs e)
+        {
+            this.Text = Utility.TitoloForm;
+        }
+    }
+}
