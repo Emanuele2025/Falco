fbec83af4c4b86fe151794b3bd18ff0ef6793b78
Emanuele2025 <emanuelemattei@tutanota.com>
2026-09-26 07:50:39 +0200

    varie fix

--- Falco/FrmLeggiFattura.cs
+++ Falco/FrmLeggiFattura.cs
@@ -297,7 +297,7 @@ namespace Falco
             }
             catch (Exception ex)
             {
-                Utility.MessaggioErrore(ex.Message);
+                Utility.MessaggioErrore(Utility.Errore + ex.Message);
             }
         }
 
